d9c3586266b12769dd46f3022a6cb985aaa7a1fb
v-frein <96353692+v-frein@users.noreply.github.com>
2022-09-29 14:25:51 +0300

    Add files via upload

---
+++ components/Navigation/Navigation.tsx
@@ -0,0 +1,30 @@
+import {
+  Box,
+  Center,
+  HStack,
+  Text,
+  Link as Chakralink,
+} from "@chakra-ui/react";
+import Link from "next/link";
+import { FC } from "react";
+
+export const Navigation: FC = () => {
+  return (
+    <Box backgroundColor="blue.500">
+      <Center>
+        <HStack w="1024px" py="6px" justifyContent="space-between">
+          <Text color="white" fontSize="32px" fontWeight="bold">
+            SBI
+          </Text>
+          <HStack gap="24px">
+            <Link href="/regions" color="white">
+              <Chakralink color="white" fontWeight="bold">
+                Регионы
+              </Chakralink>
+            </Link>
+          </HStack>
+        </HStack>
+      </Center>
+    </Box>
+  );
+};
